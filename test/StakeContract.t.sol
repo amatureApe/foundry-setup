09f9ae338c672ae4c20924a44516d2cc4602852e
amatureApe <amatureApe@gmail.com>
2022-09-22 11:28:01 -0500

    setup staking contract and tests

---
+++ test/StakeContract.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "src/StakeContract.sol";
+import "./mocks/MockERC20.sol";
+
+contract StakeContractTest is DSTest {
+    function setup() public {}
+
+    function testExaple() public {
+        assertTrue(true);
+    }
+}
